de32d79afd91e38f0e1a43345abc6f08b6e5915f
jake-ward2639 <jake.ward2639@gmail.com>
2022-03-07 13:39:38 +0000

    Tabs

---
+++ app/src/main/java/com/example/jwcalculator/FragmentCalcButtons1.java
@@ -0,0 +1,19 @@
+package com.example.jwcalculator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentCalcButtons1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calc_buttons1, container, false);
+    }
+}
