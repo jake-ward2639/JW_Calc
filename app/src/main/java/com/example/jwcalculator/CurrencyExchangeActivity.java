899859691d8e665034440eca78f72ae4878bccc5
jake-ward2639 <jake.ward2639@gmail.com>
2022-03-30 18:03:45 +0100

    Exchange Rates implemented

--- app/src/main/java/com/example/jwcalculator/CurrencyExchangeActivity.java
+++ app/src/main/java/com/example/jwcalculator/CurrencyExchangeActivity.java
@@ -1,17 +1,43 @@
 package com.example.jwcalculator;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class CurrencyExchangeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    EditText fromCurrencyEdit;
+    Spinner  fromCurrencySpinner;
+    EditText toCurrencyEdit;
+    Spinner  toCurrencySpinner;
+    String fromCurrency = "GBP";
+    String toCurrency = "EUR";
+    Float exchangeRate = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +45,17 @@ public class CurrencyExchangeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_currency_exchange);
 
         bottomNavigationView = findViewById(R.id.calMenu);
+        fromCurrencyEdit = findViewById(R.id.convertOneEdit);
+        fromCurrencySpinner = findViewById(R.id.convertOneSpinner);
+        toCurrencyEdit = findViewById(R.id.convertTwoEdit);
+        toCurrencySpinner = findViewById(R.id.convertTwoSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.currencyFrom, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//populate Spinners
+        fromCurrencySpinner.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.toCurrency, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toCurrencySpinner.setAdapter(adapter1);
 
         bottomNavigationView.setSelectedItemId(R.id.ToCE);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -49,5 +86,83 @@ public class CurrencyExchangeActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        fromCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                fromCurrency = fromCurrencySpinner.getSelectedItem().toString();
+                getAPIExchangeRate();
+                updateERActivity();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+        toCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                toCurrency = toCurrencySpinner.getSelectedItem().toString();
+                getAPIExchangeRate();
+                updateERActivity();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+        fromCurrencyEdit.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                getAPIExchangeRate();
+                updateERActivity();
+            }
+        });
+    }
+
+    public void updateERActivity(){
+        if(fromCurrencyEdit.getText().length()!=0) {
+            float result = Float.parseFloat(String.valueOf(fromCurrencyEdit.getText())) * exchangeRate;
+            Log.d("CheckF", "edit "+ String.valueOf(fromCurrencyEdit.getText()) + " ER "+exchangeRate + " result "+result);
+            toCurrencyEdit.setText(String.valueOf(result));
+        }
+    }
+
+    private void getAPIExchangeRate() {
+        if(fromCurrencyEdit.getText().length() != 0){
+            String url_str = "https://api.exchangerate.host/latest?base=" + fromCurrency + "&symbols=" + toCurrency;
+            Log.d("CheckF", "Running !!!");
+            StringRequest request = new StringRequest(Request.Method.GET, url_str,
+                    new Response.Listener()
+                    {
+                        @Override
+                        public void onResponse(Object response)
+                        {
+                            try
+                            {
+
+                                JSONObject reader = new JSONObject(response.toString());
+                                String results = reader.getJSONObject("rates").getString(toCurrency);
+                                exchangeRate = Float.parseFloat(results);
+                                Log.d("CheckF", String.valueOf(exchangeRate));
+
+                            } catch (JSONException e) { Log.d("CheckF", e.getLocalizedMessage()); }
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                            Log.d("CheckF", "FAILED! ");
+                        }
+                    });
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            queue.add(request);
+        }
     }
 }
