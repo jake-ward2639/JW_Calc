8107b719ccfb2f4d62371e9428f97147c329250c
jake-ward2639 <jake.ward2639@gmail.com>
2022-03-31 22:32:45 +0100

    Started final addition (Unit Converter)

--- app/src/main/java/com/example/jwcalculator/UnitConverterActivity.java
+++ app/src/main/java/com/example/jwcalculator/UnitConverterActivity.java
@@ -6,12 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UnitConverterActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    EditText convertLengthOneEdit;
+    Spinner convertLengthOneSpinner;
+    EditText convertLengthTwoEdit;
+    Spinner convertLengthTwoSpinner;
+    Double output;
+    final double Meters = 1;final double Kilometers = 0.001;final double Centimeters = 100;final double Millimetres = 1000;final double Micrometres = 1000000;final double Nanometres = 1000000000;final double Miles = 0.000621371;final double Yards = 1.094;final double Feet = 3.281;final double Inches = 39.37008;final double NMiles = 0.000539957;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +58,23 @@ public class UnitConverterActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        convertLengthOneEdit = findViewById(R.id.convertLengthOneEdit);
+        convertLengthOneSpinner = findViewById(R.id.convertLengthOneSpinner);
+        convertLengthTwoEdit = findViewById(R.id.convertLengthTwoEdit);
+        convertLengthTwoSpinner = findViewById(R.id.convertLengthTwoSpinner);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.lengthArray, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        convertLengthOneSpinner.setAdapter(adapter1);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.lengthArray, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        convertLengthTwoSpinner.setAdapter(adapter2);
+    }
+
+    public void convertLength(){
+        switch (convertLengthOneSpinner.getSelectedItem().toString()){
+            case "Meters":output=Double.parseDouble(convertLengthOneEdit.getText().toString());break;
+        }
     }
 }
